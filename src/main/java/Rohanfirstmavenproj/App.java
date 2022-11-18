00d9729a758d05a1c04bf55ef5f341679c16fe73
rohan <rohanparikh444@gmail.com>
2022-11-19 00:22:37 +0530

    first commit

---
+++ src/main/java/Rohanfirstmavenproj/App.java
@@ -0,0 +1,13 @@
+package Rohanfirstmavenproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Rohanfirstmavenproj/SeleniumTest.java
@@ -0,0 +1,18 @@
+package Rohanfirstmavenproj;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowswerAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+}
